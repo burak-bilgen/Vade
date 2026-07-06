4659a00c5b916890419b1ac7f328d4846fd07d9a
Burak Bilgen <119006912+burak-bilgen@users.noreply.github.com>
2026-07-06 12:47:05 +0300

    Initial Commit

---
+++ Vade/VadeApp.swift
@@ -0,0 +1,32 @@
+//
+//  VadeApp.swift
+//  Vade
+//
+//  Created by Burak on 6.07.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct VadeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
